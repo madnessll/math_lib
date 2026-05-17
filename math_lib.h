f97ca1b88146faf53e8ff075607af5646a64c887
yokatta <tolyann358@gmail.com>
2026-05-17 06:28:07 +0000

    change after homework

--- math_lib.h
+++ math_lib.h
@@ -2,68 +2,64 @@
 
 namespace math_lib {
 
-    constexpr double DOUBLE_MAX_VAL = 1.7976931348623157e+308;
-    constexpr double DOUBLE_MIN_VAL = -DOUBLE_MAX_VAL;
+    constexpr int INT_MAX_VAL = 2147483647;
+    constexpr int INT_MIN_VAL = -2147483648;
 
-    // DRY
-    inline bool checkOverflow(double res) {
-        return res > DOUBLE_MAX_VAL || res < DOUBLE_MIN_VAL;
-    }
     
-    inline bool calculateSum(double num1, double num2, double& result) {
-        double end = num1 + num2;
-        if (checkOverflow(end)) return false;
-        result = end;
+    inline bool calculateSum(int num1, int num2, double& result) {
+        if (num2 > 0 && num1 > INT_MAX_VAL - num2) return false;
+        if (num2 < 0 && num1 < INT_MIN_VAL - num2) return false;
+        result = (double)(num1 + num2);
         return true;
     }
 
-    inline bool calculateDifference(double num1, double num2, double& result) {
-        double end = num1 - num2;
-        if (checkOverflow(end)) return false;
-        result = end;
+    inline bool calculateDifference(int num1, int num2, double& result) {
+        if (num2 < 0 && num1 > INT_MAX_VAL + num2) return false;
+        if (num2 > 0 && num1 < INT_MIN_VAL + num2) return false;
+        result = (double)(num1 - num2);
         return true;
     }
 
-    inline bool calculateMult(double num1, double num2, double& result) {
-        double end = num1 * num2;
-        if (checkOverflow(end)) return false;
-        result = end;
+    inline bool calculateMult(int num1, int num2, double& result) {
+        if (num1 > 0 && num2 > 0 && num1 > INT_MAX_VAL / num2) return false;
+        if (num1 < 0 && num2 < 0 && num1 < INT_MAX_VAL / num2) return false;
+        if (num1 > 0 && num2 < 0 && num2 < INT_MIN_VAL / num1) return false;
+        if (num1 < 0 && num2 > 0 && num1 < INT_MIN_VAL / num2) return false;
+        result = (double)(num1 * num2);
         return true;
     }
 
-    inline bool calculateDivision(double num1, double num2, double& result) {
+    inline bool calculateDivision(int num1, int num2, double& result) {
         if (num2 == 0) return false;
-        double end = num1 / num2;
-        if (checkOverflow(end)) return false;
-        result = end;
+        result = (double)num1 / (double)num2;
         return true;
     }
 
-    inline bool calculatePower(double num1, double num2, double& result) {
-        if (num2 == 0) {
-            result = 1;
-            return true;
+    inline bool calculatePower(int num1, int num2, double& result) {
+        if (num2 < 0) return false;
+        if (num2 == 0) { 
+            result = 1; 
+            return true; 
         }
-        if (num2 < 0) return false; 
-        double end = 1;
-        for (int i = 0; i < (int)num2; i++) {
+        int end = 1;
+        for (int i = 0; i < num2; i++) {
+            if (end > INT_MAX_VAL / num1) return false;
             end *= num1;
-            if (checkOverflow(end)) return false;
         }
-        result = end;
+        result = (double)end;
         return true;
     }
 
-    inline bool calculateFactorial(double num, double& result) {
-        if (num < 0) return false;  
-        if (num == 0 || num == 1) {
-            result = 1;
-            return true;
+    inline bool calculateFactorial(int num1, double& result) {
+        if (num1 < 0) return false;
+        if (num1 == 0 || num1 == 1) { 
+            result = 1; 
+            return true; 
         }
         double prev;
-        if (!calculateFactorial(num - 1, prev)) return false;
-        double end = num * prev;
-        if (checkOverflow(end)) return false;
+        if (!calculateFactorial(num1 - 1, prev)) return false;
+        double end = (double)num1 * prev;
+        if (end > 1.7976931348623157e+308) return false;
         result = end;
         return true;
     }
