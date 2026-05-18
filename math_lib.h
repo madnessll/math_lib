ff85ba4e2694f2303047652e8951fbde78579ad8
yokatta <tolyann358@gmail.com>
2026-05-18 12:16:58 +0000

    some fixes[2]

--- math_lib.h
+++ math_lib.h
@@ -6,36 +6,36 @@ namespace math_lib {
     constexpr int INT_MIN_VAL = -2147483648;
 
     
-    inline bool calculateSum(int num1, int num2, double& result) {
+    inline bool calculateSum(int num1, int num2, int& result) {
         if (num2 > 0 && num1 > INT_MAX_VAL - num2) return false;
         if (num2 < 0 && num1 < INT_MIN_VAL - num2) return false;
-        result = (double)(num1 + num2);
+        result = num1 + num2;
         return true;
     }
 
-    inline bool calculateDifference(int num1, int num2, double& result) {
+    inline bool calculateDifference(int num1, int num2, int& result) {
         if (num2 < 0 && num1 > INT_MAX_VAL + num2) return false;
         if (num2 > 0 && num1 < INT_MIN_VAL + num2) return false;
-        result = (double)(num1 - num2);
+        result = num1 - num2;
         return true;
     }
 
-    inline bool calculateMult(int num1, int num2, double& result) {
+    inline bool calculateMult(int num1, int num2, int& result) {
         if (num1 > 0 && num2 > 0 && num1 > INT_MAX_VAL / num2) return false;
         if (num1 < 0 && num2 < 0 && num1 < INT_MAX_VAL / num2) return false;
         if (num1 > 0 && num2 < 0 && num2 < INT_MIN_VAL / num1) return false;
         if (num1 < 0 && num2 > 0 && num1 < INT_MIN_VAL / num2) return false;
-        result = (double)(num1 * num2);
+        result = num1 * num2;
         return true;
     }
 
-    inline bool calculateDivision(int num1, int num2, double& result) {
+    inline bool calculateDivision(int num1, int num2, int& result) {
         if (num2 == 0) return false;
-        result = (double)num1 / (double)num2;
+        result = num1 / num2;
         return true;
     }
 
-    inline bool calculatePower(int num1, int num2, double& result) {
+    inline bool calculatePower(int num1, int num2, int& result) {
         if (num2 < 0) return false;
         if (num2 == 0) { 
             result = 1; 
@@ -54,21 +54,23 @@ namespace math_lib {
             }
             end *= num1;
         }
-        result = (double)end;
+        result = end;
         return true;
     }
 
-    inline bool calculateFactorial(int num1, double& result) {
+    inline bool calculateFactorial(int num1, int& result) {
         if (num1 < 0) return false;
         if (num1 == 0 || num1 == 1) { 
             result = 1; 
             return true; 
         }
-        double prev;
+
+        int prev;
         if (!calculateFactorial(num1 - 1, prev)) return false;
-        double end = (double)num1 * prev;
-        if (end > 1.7976931348623157e+308) return false;
-        result = end;
+
+        if (prev > INT_MAX_VAL / num1) return false;
+        
+        result = num1 * prev;
         return true;
     }
 }
